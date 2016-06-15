2832a8a0ac8b9646fa5cf8c6c1230a321cc76e3c
xiongyejun <244114746@qq.com>
2016-06-15 21:03:34 +0800

    windows_alias

---
+++ 06-windows_alias.cmd
@@ -0,0 +1,43 @@
+doskey txt=C:\WINDOWS\NOTEPAD.EXE $*
+doskey ls=dir /b $*
+doskey open=start $*
+doskey txtalias=C:\WINDOWS\NOTEPAD.EXE "C:\Documents and Settings\Administrator\00-windows_alias.cmd"
+doskey runalias="C:\Documents and Settings\Administrator\00-windows_alias.cmd"
+doskey cpalias=copy "C:\Documents and Settings\Administrator\00-windows_alias.cmd" "D:\Program Files\Git\01-GitHub\99-other\06-windows_alias.cmd"
+
+doskey rm=del $*
+doskey mv=move $*
+doskey cat=type $*
+doskey cp=copy $*
+
+doskey vim=gvim $*
+doskey vi=gvim $*
+
+doskey qq="C:\Program Files\Tencent\QQ\Bin\QQScLauncher.exe"
+doskey baofeng="D:\Baofeng\StormPlayer\StormPlayer.exe"
+doskey 360ie=D:\360se6\Application\360se.exe
+doskey xunlei="D:\Thunder\Program\Thunder.exe"
+doskey vs="D:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe"
+doskey gitbash="D:\Program Files\Git\git-bash.exe" --cd-to-home
+doskey gitgui="D:\Program Files\Git\cmd\git-gui.exe"
+
+REM ´ò¿ªÍøÕ¾
+doskey baidu=start /MAX www.baidu.com
+doskey github=start /MAX github.com/xiongyejun
+
+
+doskey cdc=c:
+doskey cdd=d:
+doskey cde=e:
+doskey cdf=f:
+
+
+
+
+
+
+
+
+prompt $$ 
+
+vcvars32
